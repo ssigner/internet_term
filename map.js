fcc5c778fa8325807e993845d38baf498d7c8dd0
huni00807 <huni00807@gmail.com>
2023-06-04 06:53:01 +0900

    js 점수까지 구현, css 완성

--- map.js
+++ map.js
@@ -1,10 +1,15 @@
+const pusan_position = {
+  lat: 35.231606,
+  lng: 129.084216
+}
+
 //////////////////////////roadview////////////////////////
 
 var roadviewContainer = document.getElementById('roadview'); //로드뷰를 표시할 div
 var roadview = new kakao.maps.Roadview(roadviewContainer); //로드뷰 객체
 var roadviewClient = new kakao.maps.RoadviewClient(); //좌표로부터 로드뷰 파노ID를 가져올 로드뷰 helper객체
 
-var position = new kakao.maps.LatLng(33.450701, 126.570667);
+var position = new kakao.maps.LatLng(pusan_position.lat, pusan_position.lng);
 
 // 특정 위치의 좌표와 가까운 로드뷰의 panoId를 추출하여 로드뷰를 띄운다.
 roadviewClient.getNearestPanoId(position, 50, function (panoId) {
@@ -14,34 +19,26 @@ roadviewClient.getNearestPanoId(position, 50, function (panoId) {
 ////////////////////////////////////map/////////////////////////////
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
   mapOption = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+    center: new kakao.maps.LatLng(pusan_position.lat, pusan_position.lng), // 지도의 중심좌표
     level: 1 // 지도의 확대 레벨
   };
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
 // 지도를 클릭한 위치에 표출할 마커입니다
-var marker = new kakao.maps.Marker({
+var markers = [];
+markers.push(new kakao.maps.Marker({
   // 지도 중심좌표에 마커를 생성합니다 
   position: map.getCenter()
-});
+}));
 // 지도에 마커를 표시합니다
-marker.setMap(map);
+markers[0].setMap(map);
 
 var zoomControl = new kakao.maps.ZoomControl();
 map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
-// 지도가 확대 또는 축소되면 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
-kakao.maps.event.addListener(map, 'zoom_changed', function () {
-
-  // 지도의 현재 레벨을 얻어옵니다
-  var level = map.getLevel();
-
-  var message = '현재 지도 레벨은 ' + level + ' 입니다';
-  console.log(message);
-
-});
-
+var lat = document.getElementById('lat');
+var lng = document.getElementById('lng');
 // 지도에 클릭 이벤트를 등록합니다
 // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
 kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
@@ -50,24 +47,26 @@ kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
   var latlng = mouseEvent.latLng;
 
   // 마커 위치를 클릭한 위치로 옮깁니다
-  marker.setPosition(latlng);
-  var lat = document.getElementById('lat');
+  markers[0].setPosition(latlng);
   lat.innerHTML = latlng.getLat().toFixed(6);
-  var lng = document.getElementById('lng');
   lng.innerHTML = latlng.getLng().toFixed(6);
 });
 
 /////////////Button///////////////////////////////
 var playButton = document.getElementById("play");
+var checkButton = document.getElementById("check");
+var t_container = document.getElementById("text_container");
+var regButton = document.getElementById("register")
 var ranLat;
 var ranLng;
+//대한민국 내의 랜덤한 위도경도를 생성후 그 위도 경도를 바탕으로 로드뷰 출력
 function ranLatLng() {
   ranLat = ((Math.random() * 3) + 34.5).toFixed(6);
   ranLng = ((Math.random() * 2.5) + 126.5).toFixed(6);
   console.log(ranLat, ranLng);
   position = new kakao.maps.LatLng(ranLat, ranLng);
   roadviewClient.getNearestPanoId(position, 300, function (panoId) {
-    //panoId값이 null인 경우 핸들러
+    //panoId값이 null인 경우 재귀로 처리 -- ERROR Handling 1
     console.log(panoId);
     if(panoId == null) ranLatLng();
     else {
@@ -77,11 +76,58 @@ function ranLatLng() {
   });
   playButton.disabled = true;
 }
-
+//로드뷰 게임, 기능 1
 function playStart(){
   ranLatLng();
   console.log("last latlng ", ranLat, ranLng);
+  checkButton.disabled = false;
 }
-
 playButton.addEventListener('click', playStart);
 
+//점수 계산하는 구간
+function mapping(value, max){
+  return (value * 100.0) / max;
+}
+
+function score_calc(lat, lng){
+  var sub_lat = 3.0 - lat;
+  var sub_lng = 2.5 - lng;
+  var lat_map = mapping(sub_lat, 3.0);
+  var lng_map = mapping(sub_lng, 2.5);
+  return ((lat_map + lng_map) / 2.0).toFixed(2);
+}
+
+var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
+
+function decision(){
+  var maps_lat = parseFloat(lat.innerText);
+  var maps_lng = parseFloat(lng.innerText);
+  var score = score_calc(Math.abs(maps_lat - ranLat), Math.abs(maps_lng - ranLng));
+  document.getElementById("score").innerHTML = score;
+  var imageSize = new kakao.maps.Size(24, 35);
+  var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);  
+  var sol_marker = new kakao.maps.Marker({
+    // 지도 중심좌표에 마커를 생성합니다 
+    position: new kakao.maps.LatLng(ranLat, ranLng),
+    image : markerImage
+  });
+  markers.push(sol_marker);
+  markers[1].setMap(map);
+  checkButton.disabled = true;
+  t_container.style.display = 'inline';
+  regButton.style.display = 'inline';
+}
+
+checkButton.addEventListener('click', decision);
+
+function init(){
+  map.setCenter(new kakao.maps.LatLng(pusan_position.lat, pusan_position.lng));
+  position = new kakao.maps.LatLng(pusan_position.lat, pusan_position.lng);
+  roadviewClient.getNearestPanoId(position, 50, function (panoId) {
+    roadview.setPanoId(panoId, position);
+  });
+  markers[0].setPosition(position);
+  markers[1].setMap(null);
+}
+
+regButton.addEventListener('click', init);
